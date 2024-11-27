909ea05832d6906a0d96d8803353e16aab1d750d
Oluwakanyinsola Adebanjo-Oladipo <soladebanjo@gmail.com>
2024-11-27 00:47:57 -0500

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 def load_data(file_path):
     try:
+        # Load CSV file
         return pd.read_csv(file_path)
     except FileNotFoundError:
         print(f"Error: File not found at {file_path}")
         return None
+
